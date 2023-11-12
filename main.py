4ef3827ae5c4a691abf383542521deb1793638ac
maxmon <541182180@qq.com>
2023-11-12 23:14:39 +0800

    feat: 根据阈值来判断是否走函数

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 import shutil
 import subprocess
 import src.func.aistudio as aistudio
+import numpy as np
 
 erniebot.api_type = 'aistudio'
 erniebot.access_token = '3c410ce131fe8d246c47e26fdf932cfd44e95aa8'
@@ -37,6 +38,21 @@ functions = [
         },
     },
 ]
+f_descriptions = [f['description'] for f in functions]
+def get_embs(texts):
+  response = erniebot.Embedding.create(
+      model='ernie-text-embedding',
+      input=texts)
+  embs = [np.array(embedding) for embedding in response.get_result()]
+  embs = np.array(embs)
+  return embs
+
+f_embs = get_embs(f_descriptions)
+
+def get_sim_emb(emb, embs):
+  sim = np.dot(emb, embs.T)
+  sim = np.max(sim, axis=1)
+  return sim[0]
 
 def chat(msg, history=[], functions=[]):
   response = erniebot.ChatCompletion.create(
@@ -52,6 +68,9 @@ def chat(msg, history=[], functions=[]):
 
 def chat_python(file, need):
   os.chdir(f'.')
+  print(f'【id_{id}】start------------------', flush=True)
+  print(f'【id_{id}】file:', file, flush=True)
+  print(f'【id_{id}】need:', need, flush=True)
   if not os.path.exists('temp'):
     os.mkdir('temp')
   id = len(os.listdir('temp'))
@@ -62,21 +81,27 @@ def chat_python(file, need):
 # def pp_human_seg_v2(img_path)：人像分割，把人扣出来，背景透明
 # ---
 # 请生成一段可直接运行的python代码，用于处理以下文件，要求为【{need}】, 输入文件名为【{file}】，输出到新的文件里'''
-
   msg = msg_temp.format(need=need, file=[f.replace('\\', '/').split('/')[-1] for f in file])
   id += 1
   os.makedirs(f'temp/{id}', exist_ok=True)
   [shutil.copy(f, f'temp/{id}/') for f in file]
-  os.chdir(f'temp/{id}')
-  function_call = chat(json.dumps(file)+','+need, functions=functions)
+  emb = get_embs([need])
+  function_call = None
+  # 和已有的函数介绍相似度大于阈值，就尝试走函数调用
+  func_max_sim = get_sim_emb(emb, f_embs)
+  print(f'【id_{id}】func_max_sim:', func_max_sim, flush=True)
+  if func_max_sim > 0.55:
+    function_call = chat(json.dumps(file)+','+need, functions=functions)
   if function_call and hasattr(aistudio, function_call['name']):
     # {'name': 'get_current_temperature', 'thoughts': '用户想要知道深圳市今天的天气情况，我可以使用get_current_temperature工具来获取这个信息。', 'arguments': '{"location":"深圳市","unit":"摄氏度"}'}
+    os.chdir(f'temp/{id}')
     old_files = os.listdir('.')
     function = getattr(aistudio, function_call['name'])
     function_result = function(**json.loads(function_call['arguments']))
     answer = function_call['thoughts']
   else:
     answer = chat(msg)
+    print(f'【id_{id}】answer:', answer, flush=True)
     codes = re.findall(r'```.*\n([^犭]*?)\n```', answer, flags=re.M)
     python_code = None
     for code in codes:
@@ -90,11 +115,7 @@ def chat_python(file, need):
       'need': need,
       'file': file,
     }, indent=2, ensure_ascii=False))
-    # exec(python_code)
-    print(f'【id_{id}】start------------------', flush=True)
-    print(f'【id_{id}】file:', file, flush=True)
-    print(f'【id_{id}】need:', need, flush=True)
-    print(f'【id_{id}】answer:', answer, flush=True)
+    os.chdir(f'temp/{id}')
     old_files = os.listdir('.')
     # 安装相关依赖
     install_pkg = re.findall(r'pip install ([A-Za-z- ]*)', answer, flags=re.M)
